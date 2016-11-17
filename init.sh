c3a6ec52889f1c9a7ce2a564faa56ee334a358d0
Igor <iadergunov@gmail.com>
2016-11-17 19:02:01 +0300

    Small correction - deleting odd spaces and directives

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ﻿ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
